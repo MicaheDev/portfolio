f46cd14896e052e8a7128b80b0a65c1eb2960b74
MicaheDev <webdev.michell@gmail.com>
2024-07-17 12:50:13 -0400

    feat: complete menu animations

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -11,12 +11,7 @@ import {
 } from '@angular/core';
 import { TransitionLinkComponent } from '../transition-link/transition-link.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import {
-  remixArrowRightUpLine,
-  remixCloseLine,
-  remixHand,
-  remixMenu4Fill,
-} from '@ng-icons/remixicon';
+import { remixArrowRightUpLine, remixCloseLine, remixHand, remixMenu4Fill } from '@ng-icons/remixicon';
 import { format } from 'date-fns';
 import { TransitionRefService } from '../../../layouts/services/transition-ref.service';
 import { isPlatformBrowser } from '@angular/common';
@@ -39,8 +34,8 @@ import gsap from 'gsap';
   encapsulation: ViewEncapsulation.None,
 })
 export class NavigationComponent implements OnInit {
-  @ViewChild('gsapContainer', { static: false }) gsapContainer!: ElementRef;
-  @ViewChild('slider', { static: false }) slider!: ElementRef;
+  @ViewChild('gsapContainer', { static: false })
+  gsapContainer!: ElementRef;
 
   date!: string;
   isMenuOpen = false;
@@ -48,82 +43,66 @@ export class NavigationComponent implements OnInit {
 
   constructor(
     private refService: TransitionRefService,
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID)
+    private platformId: Object
   ) {}
 
   menuItems = [
-    {
-      path: '/',
-      label: 'Home',
-    },
-    {
-      path: '/profile',
-      label: 'profile',
-    },
-    {
-      path: '/projects',
-      label: 'Projects',
-    },
-    {
-      path: '/blog',
-      label: 'Blog',
-    },
-    {
-      path: '/contact',
-      label: 'Contact',
-    },
+    { path: '/', label: 'Home' },
+    { path: '/profile', label: 'Profile' },
+    { path: '/projects', label: 'Projects' },
+    { path: '/blog', label: 'Blog' },
+    { path: '/contact', label: 'Contact' },
   ];
 
   ngOnInit(): void {
     const date = new Date();
     this.date = format(date, 'dd/MM/yyyy'); // Formato DD/MM/YYYY
   }
-  @HostListener('window:resize', ['$event'])
+  @HostListener('window:resize')
+  onResize() {
+    this.initializeGsap();
+    this.updateGsap();
+  }
+
+  initializeGsap(): void {
+    const isMobile = window.innerWidth < 900;
+    const menuRight = isMobile ? '20px' : '40px';
+
+    gsap.context(() => {
+      gsap.set('.slider', { top: 0 });
+      gsap.set('.menu', { width: 60, height: 60, right: menuRight, top: '20px' });
+      gsap.set('.item-holder', { opacity: 0, visibility: 'hidden' });
+    }, this.gsapContainer.nativeElement);
+  }
+
+  updateGsap() {
+    const isMobile = window.innerWidth < 900;
+    const menuWidth = isMobile ? window.innerWidth : 400;
+    const menuHeight = isMobile ? window.innerHeight : 650;
+    const menuRight = isMobile ? '0px' : '30px';
+    const menuTop = isMobile ? '0px' : '10px';
+    const borderRadius = isMobile ? '0px' : '20px';
+
+      this.tl = gsap
+        .timeline({ paused: true })
+        .to('.menu', {
+          width: menuWidth,
+          height: menuHeight,
+          right: menuRight,
+          top: menuTop,
+          borderRadius,
+          duration: 0.5,
+          ease: 'power4.inOut',
+        })
+        .to('.item-holder', { visibility: 'visible', opacity: 1, stagger: 0.1, duration: 0.5 })
+        .to('.slider', { duration: 0.4, top: '-100%', ease: 'power4.inOut' });
+  }
+
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      gsap.context(() => {
-        gsap.set('.slider', {
-          top: 0,
-        });
-        gsap.set('.menu', {
-          width: 60,
-          height: 60,
-          right: window.innerWidth < 900 ? '20px' : '40px',
-          top: '20px',
-        });
-
-        gsap.set('.item-holder', {
-          opacity: 0,
-          visibility: 'hidden',
-        });
-
-        console.log(window.innerWidth);
-
-        this.tl = gsap
-          .timeline({ paused: true })
-          .to('.menu', {
-            width: window.innerWidth < 900 ? window.innerWidth : 400,
-            height: window.innerWidth < 900 ? window.innerHeight : 650,
-
-            right: window.innerWidth < 900 ? '0px' : '30px',
-            top: window.innerWidth < 900 ? '0px' : '10px',
-            borderRadius: window.innerWidth < 900 ? '0px' : '20px',
-            duration: 0.5,
-            ease: 'power4.inOut',
-          })
-          .to('.item-holder', {
-            visibility: 'visible',
-            opacity: 1,
-            stagger: 0.1,
-            duration: 0.5,
-          })
-
-          .to('.slider', {
-            duration: 0.4,
-            top: '-100%',
-            ease: 'power4.inOut',
-          });
-      }, this.gsapContainer.nativeElement);
+      this.initializeGsap();
+      this.updateGsap();
     }
   }
 
