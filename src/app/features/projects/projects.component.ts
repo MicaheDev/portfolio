34c20ffcbe6256af21221863f56ceb6960f2c020
MicaheDev <webdev.michell@gmail.com>
2024-07-20 19:36:04 -0400

    feat: add projects and improve styles

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewChecked, AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+
+type SelectedProject = {
+  index: number | null;
+  isOpen: boolean;
+};
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -7,10 +13,7 @@ import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent implements AfterViewInit {
-  constructor(
-    @Inject(PLATFORM_ID)
-    private platformId: Object
-  ) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   projects = [
     { title: 'C2 Montreal', src: '/images/c2montreal.png', color: '#000000' },
@@ -19,5 +22,16 @@ export class ProjectsComponent implements AfterViewInit {
     { title: 'Silencio', src: '/images/silencio.png', color: '#706D63' },
   ];
 
+  selectedProject: SelectedProject = { index: null, isOpen: false };
+
   ngAfterViewInit(): void {}
+
+  toggleContent(index: number) {
+    if (this.selectedProject.index === index) {
+      this.selectedProject.isOpen = !this.selectedProject.isOpen;
+    } else {
+      this.selectedProject.index = index;
+      this.selectedProject.isOpen = true;
+    }
+  }
 }
