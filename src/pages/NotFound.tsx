f5e0f68598a3124f3861f54780e41da65b7950aa
Michell Castillo <webdev.michell@gmail.com>
2025-01-19 09:16:21 -0300

    remove pages

--- src/pages/NotFound.tsx
+++
@@ -1,6 +0,0 @@
-export default function NotFound() {
-  return <div className="h-dvh w-full flex justify-center items-center">
-
-    <h1 className="text-8xl font-terminal-grotesque uppercase">404</h1>
-  </div>;
-}
